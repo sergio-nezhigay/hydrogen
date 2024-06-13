eac14f7f6e84674c7a63db97289d8ec5d9debbf1
Sergio Nezhigay <abc1971h@gmail.com>
2024-06-13 16:13:28 +0300

    change country

--- app/data/countries.ts
+++ app/data/countries.ts
@@ -1,36 +1,36 @@
 import type {Localizations} from '~/lib/type';
 
 export const countries: Localizations = {
-  default: {
-    label: 'Ukraine',
-    language: 'UK',
-    country: 'UA',
-    currency: 'UAH',
-  },
-  '/ru-ua': {
-    label: 'Russi',
-    language: 'RU',
-    country: 'UA',
-    currency: 'UAH',
-  },
-  '/en-ua': {
-    label: 'En-Ukraine',
-    language: 'EN',
-    country: 'UA',
-    currency: 'UAH',
-  },
-  '/uk-ua': {
-    label: 'Uk-Ukraine',
-    language: 'UK',
-    country: 'UA',
-    currency: 'UAH',
-  },
   //  default: {
-  //    label: 'United States (USD $)',
-  //    language: 'EN',
-  //    country: 'US',
-  //    currency: 'USD',
+  //    label: 'Ukraine',
+  //    language: 'UK',
+  //    country: 'UA',
+  //    currency: 'UAH',
   //  },
+  //  '/ru-ua': {
+  //    label: 'Russi',
+  //    language: 'RU',
+  //    country: 'UA',
+  //    currency: 'UAH',
+  //  },
+  //  '/en-ua': {
+  //    label: 'En-Ukraine',
+  //    language: 'EN',
+  //    country: 'UA',
+  //    currency: 'UAH',
+  //  },
+  //  '/uk-ua': {
+  //    label: 'Uk-Ukraine',
+  //    language: 'UK',
+  //    country: 'UA',
+  //    currency: 'UAH',
+  //  },
+  default: {
+    label: 'United States (USD $)',
+    language: 'EN',
+    country: 'US',
+    currency: 'USD',
+  },
   '/en-ad': {
     label: 'Andorra (EUR €)',
     language: 'EN',
