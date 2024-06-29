f78eaf67399eb81d1ef3fce6f982fb191d0933a3
Sergio Nezhigay <abc1971h@gmail.com>
2024-06-29 10:06:50 +0300

    add gtm base

--- app/root.tsx
+++ app/root.tsx
@@ -161,8 +161,36 @@ function Layout({children}: {children?: React.ReactNode}) {
 
         <Meta />
         <Links />
+        {/***********************************************/
+        /**********  EXAMPLE UPDATE STARTS  ************/}
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WHH5JTBZ');`,
+          }}
+        ></Script>
+        {/**********   EXAMPLE UPDATE END   ************/
+        /***********************************************/}
       </head>
       <body>
+        {/***********************************************/
+        /**********  EXAMPLE UPDATE STARTS  ************/}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WHH5JTBZ"
+            height="0"
+            width="0"
+            style={{
+              display: 'none',
+              visibility: 'hidden',
+            }}
+          ></iframe>
+        </noscript>
+        {/**********   EXAMPLE UPDATE END   ************/
+        /***********************************************/}
         {data ? (
           <Analytics.Provider
             cart={data.cart}
@@ -175,6 +203,12 @@ function Layout({children}: {children?: React.ReactNode}) {
             >
               {children}
             </PageLayout>
+            {/*<CustomAnalytics />*/}
+            {/***********************************************/
+            /**********  EXAMPLE UPDATE STARTS  ************/}
+            <GoogleTagManager />
+            {/**********   EXAMPLE UPDATE END   ************/
+            /***********************************************/}
           </Analytics.Provider>
         ) : (
           children
