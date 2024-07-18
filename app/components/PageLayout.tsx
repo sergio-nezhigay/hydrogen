14d0ec455083364dc0e8954ece6cde3f9ebd64ff
SergioNezhigay <abc1971h@gmail.com>
2024-07-18 10:50:05 +0300

    clear footer

--- app/components/PageLayout.tsx
+++ app/components/PageLayout.tsx
@@ -429,12 +429,7 @@ function Footer({menu}: {menu?: EnhancedMenu}) {
         bg-primary dark:bg-contrast dark:text-primary text-contrast overflow-hidden`}
     >
       <FooterMenu menu={menu} />
-      <div
-        className={`self-end pt-8 opacity-50 md:col-span-2 lg:col-span-${itemsCount}`}
-      >
-        &copy; {new Date().getFullYear()} / Shopify, Inc. Hydrogen is an MIT
-        Licensed Open Source project.
-      </div>
+
     </Section>
   );
 }
