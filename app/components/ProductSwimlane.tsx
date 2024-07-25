03a085607a20f7ad9cb56ab6d85d3f7213a55be3
Sergio Nezhigay <abc1971h@gmail.com>
2024-07-25 16:34:10 +0300

    setup title

--- app/components/ProductSwimlane.tsx
+++ app/components/ProductSwimlane.tsx
@@ -26,21 +26,27 @@ export function ProductSwimlane({
   ...props
 }: ProductSwimlaneProps) {
   return (
-    //<Section heading={title} padding="y" {...props}>
-    <Carousel className="">
-      <CarouselContent className="-ml-4">
-        {products.nodes.map((product) => (
-          <CarouselItem
-            key={product.id}
-            className=" pl-4  md:basis-1/4 lg:basis-1/5 xl:basis-1/6"
-          >
-            <ProductCard product={product} className="w-full" />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      {/*<CarouselPrevious />
+    <Section
+      heading={title}
+      {...props}
+      padding="y"
+      display="flex"
+      className="flex flex-col"
+    >
+      <Carousel className="">
+        <CarouselContent className="-ml-4">
+          {products.nodes.map((product) => (
+            <CarouselItem
+              key={product.id}
+              className=" pl-4 sm:basis-1/2  md:basis-1/4 lg:basis-1/5 xl:basis-1/6"
+            >
+              <ProductCard product={product} className="w-full" />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        {/*<CarouselPrevious />
       <CarouselNext />*/}
-    </Carousel>
-    //</Section>
+      </Carousel>
+    </Section>
   );
 }
