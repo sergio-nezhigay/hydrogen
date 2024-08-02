bbab3a7f94baaa1fef9480dda7a5284483e3706a
Sergio Nezhigay <abc1971h@gmail.com>
2024-08-02 11:43:59 +0300

    comment gtm events

--- app/modules/CustomAnalytics.tsx
+++ app/modules/CustomAnalytics.tsx
@@ -15,40 +15,40 @@ export function CustomAnalytics() {
       console.log('CustomAnalytics - isTrackingAllowed', isTrackingAllowed);
     }, 1000);
     // Standard events
-    subscribe('page_viewed', (data) => {
-      // eslint-disable-next-line no-console
-      console.log('CustomAnalytics - Page viewed:', data);
-      initializeDataLayer();
-      window.dataLayer.push({
-        event: 'shopify_page_view',
-        page: data.url,
-      });
-    });
+    //subscribe('page_viewed', (data) => {
+    //  // eslint-disable-next-line no-console
+    //  console.log('CustomAnalytics - Page viewed:', data);
+    //  initializeDataLayer();
+    //  window.dataLayer.push({
+    //    event: 'shopify_page_view',
+    //    page: data.url,
+    //  });
+    //});
 
-    subscribe('product_viewed', (data) => {
-      // eslint-disable-next-line no-console
-      console.log('CustomAnalytics - Product viewed:', data);
-      initializeDataLayer();
-      if (data.products && data.products.length > 0) {
-        const product = data.products[0];
-        window.dataLayer.push({
-          event: 'view_item',
-          ecommerce: {
-            items: [
-              {
-                item_id: product.id,
-                item_name: product.title,
-                price: product.price,
-                quantity: product.quantity,
-                item_variant: product.variantTitle,
-                item_brand: product.vendor,
-                // Add more product details as needed
-              },
-            ],
-          },
-        });
-      }
-    });
+    //subscribe('product_viewed', (data) => {
+    //  // eslint-disable-next-line no-console
+    //  console.log('CustomAnalytics - Product viewed:', data);
+    //  initializeDataLayer();
+    //  if (data.products && data.products.length > 0) {
+    //    const product = data.products[0];
+    //    window.dataLayer.push({
+    //      event: 'view_item',
+    //      ecommerce: {
+    //        items: [
+    //          {
+    //            item_id: product.id,
+    //            item_name: product.title,
+    //            price: product.price,
+    //            quantity: product.quantity,
+    //            item_variant: product.variantTitle,
+    //            item_brand: product.vendor,
+    //            // Add more product details as needed
+    //          },
+    //        ],
+    //      },
+    //    });
+    //  }
+    //});
     // subscribe('collection_viewed', (data) => {
     //   console.log('CustomAnalytics - Collection viewed:', data);
     // });
