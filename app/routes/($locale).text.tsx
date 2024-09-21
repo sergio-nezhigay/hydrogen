83ff470e6b1f04f8c625907d701308dce19270c5
Sergio Nezhigay <abc1971h@gmail.com>
2024-09-21 12:43:58 +0300

    add log

--- app/routes/($locale).text.tsx
+++ app/routes/($locale).text.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 function text() {
+  console.log(
+    '===== LOG START =====',
+    new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}),
+  );
+  //console.log('myVariable:', JSON.stringify(myVariable, null, 4));
   return <div>=====================text</div>;
 }
 
