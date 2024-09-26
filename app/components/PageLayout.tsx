4aa621f5f544a9accc9315e098983473d187d447
Sergio Nezhigay <abc1971h@gmail.com>
2024-09-26 12:20:35 +0300

    search step1

--- app/components/PageLayout.tsx
+++ app/components/PageLayout.tsx
@@ -78,7 +78,7 @@ function SearchAside() {
 
   return (
     <Aside type="search" heading={translation.search}>
-      <div className="h-[calc(100vh_-_var(--header-height)_-_40px)] overflow-y-auto">
+      <div className="h-[calc(100vh_-_var(--header-height)_-_200px)] overflow-y-auto">
         <SearchFormPredictive>
           {({fetchResults, goToSearch, inputRef}) => (
             <div className="flex gap-2 items-center">
