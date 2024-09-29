c34135b7d02e232fe66a85f1785fa378b9b6fcd0
Sergio Nezhigay <abc1971h@gmail.com>
2024-09-29 15:15:06 +0300

    change menu to accordion

--- app/modules/MobileNavigationMenu.tsx
+++ app/modules/MobileNavigationMenu.tsx
@@ -1,8 +1,11 @@
-//'use client';
-
-import {Disclosure} from '@headlessui/react';
 import {Link} from '@remix-run/react';
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '~/components/ui/accordion';
 import {useAside} from '~/components/Aside';
 import {navigationData} from '~/data/navigationData';
 
@@ -11,54 +14,52 @@ export const MobileNavigationMenu = () => {
 
   return (
     <div className="p-4 space-y-2 flex flex-col test">
-      {navigationData.map((menuGroup) => (
-        <Disclosure key={menuGroup.title}>
-          {({open}) => (
-            <>
-              {menuGroup.items ? (
-                <>
-                  <Disclosure.Button
-                    className={`w-full text-left p-4 font-bold rounded-md  transition-colors duration-200 ${
-                      open ? 'bg-gray-200 ' : ''
-                    } hover:bg-gray-200`}
-                  >
-                    {menuGroup.title}
-                  </Disclosure.Button>
-                  <Disclosure.Panel
-                    className={`transition-max-height duration-500 ease-out overflow-hidden ${
-                      open ? 'max-h-96' : 'max-h-0'
-                    }`}
-                  >
-                    <ul className="pl-4 space-y-1">
-                      {menuGroup.items.map((item) => (
-                        <li key={item.title}>
-                          <Link
-                            to={item.to}
-                            prefetch="viewport"
-                            className="block p-2 text-gray-700 rounded transition-colors duration-200 hover:bg-gray-200"
-                            onClick={close}
-                          >
-                            {item.title}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </Disclosure.Panel>
-                </>
-              ) : (
-                <Link
-                  to={menuGroup.to!}
-                  prefetch="viewport"
-                  className="block p-4 text-gray-700 rounded transition-colors duration-200 hover:bg-gray-200"
-                  onClick={close}
+      <Accordion type="single" collapsible>
+        {navigationData.map((menuGroup) => (
+          <AccordionItem key={menuGroup.title} value={menuGroup.title}>
+            {menuGroup.items ? (
+              <>
+                <AccordionTrigger
+                  className={`w-full text-left p-4 font-bold rounded-md transition-colors duration-200
+                    hover:bg-gray-100
+                    data-[state=open]:text-blue-700
+
+          `}
                 >
                   {menuGroup.title}
-                </Link>
-              )}
-            </>
-          )}
-        </Disclosure>
-      ))}
+                </AccordionTrigger>
+                <AccordionContent
+                  className={`transition-max-height duration-500 ease-out overflow-hidden `}
+                >
+                  <ul className="pl-4 space-y-1">
+                    {menuGroup.items.map((item) => (
+                      <li key={item.title}>
+                        <Link
+                          to={item.to}
+                          prefetch="viewport"
+                          className="block p-2 text-gray-700 rounded transition-colors duration-200 hover:bg-gray-100"
+                          onClick={close}
+                        >
+                          {item.title}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </AccordionContent>
+              </>
+            ) : (
+              <Link
+                to={menuGroup.to!}
+                prefetch="viewport"
+                className="block p-4 text-gray-700 rounded transition-colors duration-200 hover:bg-gray-100"
+                onClick={close}
+              >
+                {menuGroup.title}
+              </Link>
+            )}
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
   );
 };
