f56d76a8b38b47ab617957ade60abb20bc29ca30
Sergio Nezhigay <abc1971h@gmail.com>
2024-10-03 10:33:34 +0300

    add test route

--- app/components/ui/navigation-menu.tsx
+++ app/components/ui/navigation-menu.tsx
@@ -1,3 +1,5 @@
+//navigation menu
+
 import * as React from 'react';
 import * as NavigationMenuPrimitive from '@radix-ui/react-navigation-menu';
 import {cva} from 'class-variance-authority';
--- app/routes/($locale).text.tsx
+++ app/routes/($locale).text.tsx
@@ -1,12 +1,108 @@
+//import './styles.css';
 import React from 'react';
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 
-function text() {
-  console.log(
-    '===== LOG START =====',
-    new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'}),
+export default function App() {
+  const [offset, setOffset] = React.useState<number | null>(null);
+  const listRef = React.useRef<HTMLUListElement>(null);
+  const [value, setValue] = React.useState<string | undefined>(undefined);
+  const [activeTrigger, setActiveTrigger] =
+    React.useState<HTMLButtonElement | null>(null);
+
+  React.useEffect(() => {
+    const list = listRef.current;
+    if (activeTrigger && list) {
+      const listWidth = list.offsetWidth;
+      const listCenter = listWidth / 2;
+
+      const triggerOffsetRight =
+        listWidth -
+        activeTrigger.offsetLeft -
+        activeTrigger.offsetWidth +
+        activeTrigger.offsetWidth / 2;
+
+      setOffset(Math.round(listCenter - triggerOffsetRight));
+    } else if (value === '') {
+      setOffset(null);
+    }
+  }, [activeTrigger, value]);
+
+  return (
+    <NavigationMenu.Root
+      value={value}
+      onValueChange={setValue}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        position: 'relative',
+      }}
+    >
+      <NavigationMenu.List
+        ref={listRef}
+        style={{
+          display: 'flex',
+          listStyle: 'none',
+          margin: 0,
+          marginBottom: 15,
+          padding: 0,
+        }}
+      >
+        {['one', 'two', 'three loooong', 'four', 'asdfgefg', 'f'].map(
+          (item) => {
+            return (
+              <NavigationMenu.Item key={item} value={item}>
+                <NavigationMenu.Trigger
+                  ref={(node) => {
+                    if (item === value && activeTrigger !== node) {
+                      setActiveTrigger(node);
+                    }
+                    return node;
+                  }}
+                >
+                  {item}
+                </NavigationMenu.Trigger>
+                <NavigationMenu.Content
+                  style={{width: 100, height: 100, backgroundColor: 'red'}}
+                >
+                  <button>{item} content</button>
+                </NavigationMenu.Content>
+              </NavigationMenu.Item>
+            );
+          },
+        )}
+
+        <NavigationMenu.Indicator
+          style={{
+            bottom: 0,
+            height: 5,
+            backgroundColor: 'grey',
+            transition: 'all 0.5s ease',
+          }}
+        />
+      </NavigationMenu.List>
+
+      <div
+        style={{
+          position: 'absolute',
+          left: 0,
+          top: '100%',
+          width: '100%',
+          backgroundColor: 'yellow',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <NavigationMenu.Viewport
+          style={{
+            display: !offset ? 'none' : undefined,
+            transform: `translateX(${offset}px)`,
+            top: '100%',
+            width: 'var(--radix-navigation-menu-viewport-width)',
+            transition: 'all 0.5s ease',
+            backgroundColor: 'red',
+          }}
+        />
+      </div>
+    </NavigationMenu.Root>
   );
-  //console.log('myVariable:', JSON.stringify(myVariable, null, 4));
-  return <div>=====================text</div>;
 }
-
-export default text;
