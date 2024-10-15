534feff6b71afe71d717fc6d2bb0b371452af57e
Sergio Nezhigay <abc1971h@gmail.com>
2024-10-15 20:37:10 +0300

    correct csp

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -36,6 +36,7 @@ export default async function handleRequest(
       'https://*.googletagmanager.com',
       'https://*.googleadservices.com',
       'https://*.google.com.ua',
+      'https://google.com',
       'https://*.google.com',
     ],
     frameSrc: [
