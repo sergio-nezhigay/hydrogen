4f7ba63aef251d735d3f767b1f730875fecf54d6
Sergio Nezhigay <abc1971h@gmail.com>
2024-10-24 18:06:02 +0300

    translate order page

--- app/routes/($locale).account.orders.$id copy.tsx
+++ app/routes/($locale).account.orders.$id copy.tsx
@@ -1,16 +1,15 @@
 import clsx from 'clsx';
 import {json, redirect, type LoaderFunctionArgs} from '@shopify/remix-oxygen';
 import {useLoaderData, type MetaFunction} from '@remix-run/react';
-import {Image, flattenConnection} from '@shopify/hydrogen';
+import {Money, Image, flattenConnection} from '@shopify/hydrogen';
 import type {FulfillmentStatus} from '@shopify/hydrogen/customer-account-api-types';
 
 import {translations} from '~/data/translations';
 import type {OrderFragment} from 'customer-accountapi.generated';
 import {statusMessage} from '~/lib/utils';
 import {Link} from '~/components/Link';
-import {Heading, Section, Text} from '~/components/Text';
+import {Heading, PageHeader, Text} from '~/components/Text';
 import {CUSTOMER_ORDER_QUERY} from '~/graphql/customer-account/CustomerOrderQuery';
-import {HryvniaMoney} from '~/components/HryvniaMoney';
 
 export const meta: MetaFunction<typeof loader> = ({data}) => {
   return [{title: `Order ${data?.order?.name}`}];
@@ -96,23 +95,19 @@ export default function OrderRoute() {
     translation,
   } = useLoaderData<typeof loader>();
   return (
-    <Section
-      heading="Деталі замовлення"
-      headingClassName="text-center mx-auto"
-      padding="y"
-      useH1
-      className="py-8"
-    >
-      <Link to="/account">
-        <Text color="subtle">Перейти в деталі акаунта</Text>
-      </Link>
+    <div>
+      <PageHeader heading="Order detail">
+        <Link to="/account">
+          <Text color="subtle">Return to Account Overview</Text>
+        </Link>
+      </PageHeader>
       <div className="w-full p-6 sm:grid-cols-1 md:p-8 lg:p-12 lg:py-6">
         <div>
           <Text as="h3" size="lead">
-            Замовлення №{order.name}
+            Order No. {order.name}
           </Text>
           <Text className="mt-2" as="p">
-            Дата обробки {new Date(order.processedAt!).toDateString()}
+            Placed on {new Date(order.processedAt!).toDateString()}
           </Text>
           <div className="grid items-start gap-12 sm:grid-cols-1 md:grid-cols-4 md:gap-16 sm:divide-y sm:divide-gray-200">
             <table className="min-w-full my-8 divide-y divide-gray-300 md:col-span-3">
@@ -122,25 +117,25 @@ export default function OrderRoute() {
                     scope="col"
                     className="pb-4 pl-0 pr-3 font-semibold text-left"
                   >
-                    Товар
+                    Product
                   </th>
                   <th
                     scope="col"
                     className="hidden px-4 pb-4 font-semibold text-right sm:table-cell md:table-cell"
                   >
-                    Ціна
+                    Price
                   </th>
                   <th
                     scope="col"
                     className="hidden px-4 pb-4 font-semibold text-right sm:table-cell md:table-cell"
                   >
-                    Кількість
+                    Quantity
                   </th>
                   <th
                     scope="col"
                     className="px-4 pb-4 font-semibold text-right"
                   >
-                    Загалом
+                    Total
                   </th>
                 </tr>
               </thead>
@@ -165,7 +160,7 @@ export default function OrderRoute() {
                           </Text>
                         </div>
                         <dl className="grid">
-                          <dt className="sr-only">Товар</dt>
+                          <dt className="sr-only">Product</dt>
                           <dd className="truncate lg:hidden">
                             <Heading size="copy" format as="h3">
                               {lineItem.title}
@@ -174,31 +169,30 @@ export default function OrderRoute() {
                               {lineItem.variantTitle}
                             </Text>
                           </dd>
-                          <dt className="sr-only">Ціна</dt>
+                          <dt className="sr-only">Price</dt>
                           <dd className="truncate sm:hidden">
                             <Text size="fine" className="mt-4">
-                              <HryvniaMoney data={lineItem.price!} />
+                              <Money data={lineItem.price!} />
                             </Text>
                           </dd>
-                          <dt className="sr-only">Кількість</dt>
+                          <dt className="sr-only">Quantity</dt>
                           <dd className="truncate sm:hidden">
                             <Text className="mt-1" size="fine">
-                              К-ть: {lineItem.quantity}
+                              Qty: {lineItem.quantity}
                             </Text>
                           </dd>
                         </dl>
                       </div>
                     </td>
                     <td className="hidden px-3 py-4 text-right align-top sm:align-middle sm:table-cell">
-                      <HryvniaMoney data={lineItem.price!} />
+                      <Money data={lineItem.price!} />
                     </td>
                     <td className="hidden px-3 py-4 text-right align-top sm:align-middle sm:table-cell">
                       {lineItem.quantity}
                     </td>
                     <td className="px-3 py-4 text-right align-top sm:align-middle sm:table-cell">
                       <Text>
-                        <HryvniaMoney data={lineItem.totalDiscount!} />
-                        {/*<Money data={lineItem.totalDiscount!} />*/}
+                        <Money data={lineItem.totalDiscount!} />
                       </Text>
                     </td>
                   </tr>
@@ -213,22 +207,21 @@ export default function OrderRoute() {
                       colSpan={3}
                       className="hidden pt-6 pl-6 pr-3 font-normal text-right sm:table-cell md:pl-0"
                     >
-                      <Text>Знижки</Text>
+                      <Text>Discounts</Text>
                     </th>
                     <th
                       scope="row"
                       className="pt-6 pr-3 font-normal text-left sm:hidden"
                     >
-                      <Text>Знижки</Text>
+                      <Text>Discounts</Text>
                     </th>
                     <td className="pt-6 pl-3 pr-4 font-medium text-right text-green-700 md:pr-3">
                       {discountPercentage ? (
                         <span className="text-sm">
-                          -{discountPercentage}% дешевше
+                          -{discountPercentage}% OFF
                         </span>
                       ) : (
-                        discountValue && <HryvniaMoney data={discountValue!} />
-                        //discountValue && <Money data={discountValue!} />
+                        discountValue && <Money data={discountValue!} />
                       )}
                     </td>
                   </tr>
@@ -248,33 +241,50 @@ export default function OrderRoute() {
                     <Text>{translation.subtotal}</Text>
                   </th>
                   <td className="pt-6 pl-3 pr-4 text-right md:pr-3">
-                    <HryvniaMoney data={order.subtotal!} />
+                    <Money data={order.subtotal!} />
                   </td>
                 </tr>
-
+                <tr>
+                  <th
+                    scope="row"
+                    colSpan={3}
+                    className="hidden pt-4 pl-6 pr-3 font-normal text-right sm:table-cell md:pl-0"
+                  >
+                    Tax
+                  </th>
+                  <th
+                    scope="row"
+                    className="pt-4 pr-3 font-normal text-left sm:hidden"
+                  >
+                    <Text>Tax</Text>
+                  </th>
+                  <td className="pt-4 pl-3 pr-4 text-right md:pr-3">
+                    <Money data={order.totalTax!} />
+                  </td>
+                </tr>
                 <tr>
                   <th
                     scope="row"
                     colSpan={3}
                     className="hidden pt-4 pl-6 pr-3 font-semibold text-right sm:table-cell md:pl-0"
                   >
-                    Усього
+                    Total
                   </th>
                   <th
                     scope="row"
                     className="pt-4 pr-3 font-semibold text-left sm:hidden"
                   >
-                    <Text>Загалом</Text>
+                    <Text>Total</Text>
                   </th>
                   <td className="pt-4 pl-3 pr-4 font-semibold text-right md:pr-3">
-                    <HryvniaMoney data={order.totalPrice!} />
+                    <Money data={order.totalPrice!} />
                   </td>
                 </tr>
               </tfoot>
             </table>
             <div className="sticky border-none top-nav md:my-8">
               <Heading size="copy" className="font-semibold" as="h3">
-                Адреса доставки
+                Shipping Address
               </Heading>
               {order?.shippingAddress ? (
                 <ul className="mt-6">
@@ -292,10 +302,10 @@ export default function OrderRoute() {
                   )}
                 </ul>
               ) : (
-                <p className="mt-3">Адреса не заповнена</p>
+                <p className="mt-3">No shipping address defined</p>
               )}
               <Heading size="copy" className="mt-8 font-semibold" as="h3">
-                Стан
+                Status
               </Heading>
               {fulfillmentStatus && (
                 <div
@@ -313,6 +323,6 @@ export default function OrderRoute() {
           </div>
         </div>
       </div>
-    </Section>
+    </div>
   );
 }
