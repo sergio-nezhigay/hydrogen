8f8a3c11b60f65c4cad14913b13c8c0b4eacce3b
Sergio Nezhigay <abc1971h@gmail.com>
2024-11-06 13:14:06 +0200

    Deactivate GTM event subscriptions for page views
    GTM event subscriptions for 'product_viewed' and 'page_viewed' have been commented out, likely to reconsider their implementation or prevent redundant data during testing or debugging.

--- app/modules/GoogleTagManager.tsx
+++ app/modules/GoogleTagManager.tsx
@@ -12,11 +12,17 @@ export function GoogleTagManager() {
   const {ready} = register('Google Tag Manager');
 
   useEffect(() => {
-    subscribe('product_viewed', () => {
-      // Triggering a custom event in GTM when a product is viewed
-      window.dataLayer.push({event: 'viewed-product'});
-    });
-
+    //subscribe('product_viewed', () => {
+    //  // Triggering a custom event in GTM when a product is viewed
+    //  window.dataLayer.push({event: 'viewed-product'});
+    //});
+    //subscribe('page_viewed', (data) => {
+    //  console.log('CustomAnalytics - Page viewed:', data);
+    //  window.dataLayer.push({
+    //    event: 'shopify_page_view',
+    //    page: data.url,
+    //  });
+    //});
     ready();
   }, []);
 
