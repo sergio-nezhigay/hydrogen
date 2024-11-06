11539a83170e151b306be64aabd3d748ac418200
Sergio Nezhigay <abc1971h@gmail.com>
2024-11-06 13:35:46 +0200

    Enable GTM events for product and page views

--- app/modules/GoogleTagManager.tsx
+++ app/modules/GoogleTagManager.tsx
@@ -1,4 +1,5 @@
 import {useAnalytics} from '@shopify/hydrogen';
+import {subscribe} from 'graphql';
 import {useEffect} from 'react';
 
 declare global {
@@ -8,21 +9,21 @@ declare global {
 }
 
 export function GoogleTagManager() {
-  const {register} = useAnalytics();
+  const {register, subscribe} = useAnalytics();
   const {ready} = register('Google Tag Manager');
 
   useEffect(() => {
-    //subscribe('product_viewed', () => {
-    //  // Triggering a custom event in GTM when a product is viewed
-    //  window.dataLayer.push({event: 'viewed-product'});
-    //});
-    //subscribe('page_viewed', (data) => {
-    //  console.log('CustomAnalytics - Page viewed:', data);
-    //  window.dataLayer.push({
-    //    event: 'shopify_page_view',
-    //    page: data.url,
-    //  });
-    //});
+    subscribe('product_viewed', () => {
+      // Triggering a custom event in GTM when a product is viewed
+      window.dataLayer.push({event: 'viewed-product'});
+    });
+    subscribe('page_viewed', (data) => {
+      console.log('CustomAnalytics - Page viewed:', data);
+      window.dataLayer.push({
+        event: 'shopify_page_view',
+        page: data.url,
+      });
+    });
     ready();
   }, []);
 
