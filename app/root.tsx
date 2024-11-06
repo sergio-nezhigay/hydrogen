7e31ad7fbe3eec39929a58c4219cadd3e09ca0ce
Sergio Nezhigay <abc1971h@gmail.com>
2024-11-06 13:20:32 +0200

    Temporarily disable Google Tag Manager scripts
    Commented out Google Tag Manager script and noscript sections, likely for debugging or testing purposes. This adjustment might be made to prevent tracking while modifications or analyses are being performed.

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/iframe-has-title */
 import type {SeoConfig} from '@shopify/hydrogen';
 import {
   useNonce,
@@ -160,7 +159,7 @@ export function Layout({children}: {children?: React.ReactNode}) {
         <Links />
         {/***********************************************/
         /**********  EXAMPLE UPDATE STARTS  ************/}
-        <Script
+        {/*<Script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -168,14 +167,14 @@ export function Layout({children}: {children?: React.ReactNode}) {
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-WRQRP5RF');`,
           }}
-        ></Script>
+        ></Script>*/}
         {/**********   EXAMPLE UPDATE END   ************/
         /***********************************************/}
       </head>
       <body>
         {/***********************************************/
         /**********  EXAMPLE UPDATE STARTS  ************/}
-        <noscript>
+        {/*<noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-WRQRP5RF"
             height="0"
@@ -185,7 +184,7 @@ export function Layout({children}: {children?: React.ReactNode}) {
               visibility: 'hidden',
             }}
           ></iframe>
-        </noscript>
+        </noscript>*/}
         {/**********   EXAMPLE UPDATE END   ************/
         /***********************************************/}
         {data ? (
@@ -198,7 +197,7 @@ export function Layout({children}: {children?: React.ReactNode}) {
             {/*<CustomAnalytics />*/}
             {/***********************************************/
             /**********  EXAMPLE UPDATE STARTS  ************/}
-            <GoogleTagManager />
+            {/*<GoogleTagManager />*/}
             {/**********   EXAMPLE UPDATE END   ************/
             /***********************************************/}
           </Analytics.Provider>
