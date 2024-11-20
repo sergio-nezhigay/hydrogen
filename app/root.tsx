d67278329ad3e9d2016a8ab7894f27811229c5e9
Sergio Nezhigay <abc1971h@gmail.com>
2024-11-20 22:33:39 +0200

    change cookieDomain

--- app/root.tsx
+++ app/root.tsx
@@ -193,7 +193,7 @@ export function Layout({children}: {children?: React.ReactNode}) {
             cart={data.cart}
             shop={data.shop}
             consent={data.consent}
-            cookieDomain="c2da09-15.myshopify.com"
+            cookieDomain="informatica.com.ua"
           >
             <PageLayout {...data}>{children}</PageLayout>
             {/*<CustomAnalytics />*/}
