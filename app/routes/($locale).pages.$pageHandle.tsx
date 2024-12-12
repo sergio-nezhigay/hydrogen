b4b5433013b86c9354398e552b606763e86358bd
Sergio Nezhigay <abc1971h@gmail.com>
2024-12-12 17:34:30 +0200

    update padding b

--- app/routes/($locale).pages.$pageHandle.tsx
+++ app/routes/($locale).pages.$pageHandle.tsx
@@ -41,7 +41,7 @@ export default function Page() {
 
   return (
     <>
-      <Section>
+      <Section className="!pb-20">
         <PageHeader
           heading={page.title}
           variant="allCollections"
