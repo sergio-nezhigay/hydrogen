0e62163e3bfb65cda0232f17b5919c8890a2308c
Sergio Nezhigay <abc1971h@gmail.com>
2025-01-20 10:27:37 +0200

    update-title-test

--- app/routes/($locale).products.$handle.tsx
+++ app/routes/($locale).products.$handle.tsx
@@ -209,7 +209,7 @@ export default function Product() {
           <div className="flex flex-col gap-8 md:gap-16 md:top-24 md:sticky ">
             <div className="grid gap-2 ">
               <Heading as="h1" className=" overflow-hidden whitespace-normal ">
-                {title}
+                {title}-1
               </Heading>
               <div
                 className={clsx({
