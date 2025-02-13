b0fa8589ea7666692738b1c7c20fec48761e8e35
Sergio Nezhigay <abc1971h@gmail.com>
2025-02-13 12:42:56 +0200

    Add visited products tracking with localStorage functionality

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -74,7 +74,7 @@ export function Aside({
           className="text-white absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary"
         >
           <button type="submit" onClick={close}>
-            <X className="h-4 w-4" onClick={close} />
+            <X className="h-4 w-4" />
             <span className="sr-only">Close</span>
           </button>
         </SheetClose>
